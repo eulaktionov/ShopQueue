b45be8e8c452ce30e3c662ba8f91e3d173d3b374
eugeneLuck <eugeneLuck@gmail.com>
2023-12-27 11:55:26 +0300

    Add project files.

---
+++ QueueModel/Shop.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace QueueModel
+{
+    public class Shop
+    {
+        //static ModelTimer modelTimer = new ModelTimer();
+        System.Timers.Timer modelTimer = new System.Timers.Timer(1000);
+        int _modelTime = 0;
+        public Shop()
+        {
+            //modelTimer.OnTimeInterval += (s, e) => Debug.WriteLine
+            //    ((e as ModelTimeEventArgs).ModelTime.ToString());
+            modelTimer.Elapsed += ModelTimer_Elapsed;
+            modelTimer.Start();
+        }
+
+        private void ModelTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            Debug.WriteLine((++_modelTime).ToString());
+        }
+    }
+
+    public class ModelTimer : System.Timers.Timer
+    {
+        public event EventHandler OnTimeInterval;
+        int _modelTime = 0;
+        public ModelTimer() : base(1000)
+        {
+            Elapsed += (s, e) => OnTimeInterval
+                (this, new ModelTimeEventArgs { ModelTime = ++_modelTime });
+        }
+        public override 
+    }
+
+    public class ModelTimeEventArgs : EventArgs
+    {
+        public int ModelTime { get; set; }
+    }
+}
---
+++ ShopQueue/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ShopQueue
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
